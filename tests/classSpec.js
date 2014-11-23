e65c4e185d46879040ec7cc39345a17c580796d9
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-11-23 16:25:05 +0530

    Add a simple test spec for checking Applait.Finder class integrity
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

---
+++ tests/classSpec.js
@@ -0,0 +1,37 @@
+describe("Applait.Finder class", function () {
+
+    var finder;
+
+    beforeEach(function () {
+        finder = new Applait.Finder();
+    });
+
+    it("should exist.", function () {
+        expect(Applait).toBeDefined();
+        expect(Applait.Finder).toBeDefined();
+        expect(finder).toBeDefined();
+    });
+
+    it("should be a constructor", function () {
+        expect((new Applait.Finder()) instanceof Applait.Finder).toBe(true);
+    });
+
+    it("should inherit from EventEmitter.", function () {
+        expect(Applait.Finder.prototype instanceof EventEmitter).toBe(true);
+    });
+
+    it("should have sane default values.", function (){
+        expect(finder.options).toBeDefined();
+        expect(finder.type).toBe("sdcard");
+        expect(finder.minsearchlength).toBe(3);
+        expect(finder.casesensitive).toBe(false);
+        expect(finder.debugmode).toBe(false);
+        expect(finder.hidden).toBe(false);
+    });
+
+    it("should have default properties set up with proper values.", function () {
+        expect(finder.searchcompletecount).toBe(0);
+        expect(finder.filematchcount).toBe(0);
+        expect(finder.searchkey).toBe("");
+    });
+});
