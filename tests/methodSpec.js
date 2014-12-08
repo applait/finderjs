83b1f5ff7dcfa1ad7bafb8acc502510b238bd6a4
Jaipradeesh <jaipradeesh@gmail.com>
2014-12-08 19:55:03 +0530

    Fixes #4: Added tests for checkhidden method

--- tests/methodSpec.js
+++ tests/methodSpec.js
@@ -11,3 +11,40 @@ describe("Applait.Finder.prototype splitname function", function () {
         expect(splitname.path).toBe("/sdcard/DCIM/100MZLLA");
     });
 });
+
+describe("Applait.Finder.prototype.checkhidden method", function() {
+
+    describe("when this.hidden is true", function () {
+
+        var finder = new Applait.Finder({ hidden: true });
+
+        it("should return true when file path has hidden elements.", function (){
+            expect(finder.checkhidden("/sdcard/DCIM/100MZLLA/.1.jpg")).toBe(true);
+            expect(finder.checkhidden("/sdcard/DCIM/.100MZLLA/.1.jpg")).toBe(true);
+            expect(finder.checkhidden("/sdcard/DCIM/.100MZLLA/1.jpg")).toBe(true);
+        });
+
+
+        it("should return true when file path does not have hidden elements.", function (){
+            expect(finder.checkhidden("/sdcard/DCIM/100MZLLA/1.jpg")).toBe(true);
+        });
+
+    });
+
+    describe("when this.hidden is false", function () {
+
+        var finder = new Applait.Finder({ hidden: false });
+
+        it("should return true when file path does not have hidden elements.", function (){
+            expect(finder.checkhidden("/sdcard/DCIM/100MZLLA/1.jpg")).toBe(true);
+        });
+
+        it("should return false when file path has hidden elements.", function (){
+            expect(finder.checkhidden("/sdcard/DCIM/100MZLLA/.1.jpg")).toBe(false);
+            expect(finder.checkhidden("/sdcard/DCIM/.100MZLLA/.1.jpg")).toBe(false);
+            expect(finder.checkhidden("/sdcard/DCIM/.100MZLLA/1.jpg")).toBe(false);
+        });
+
+    });
+
+});
